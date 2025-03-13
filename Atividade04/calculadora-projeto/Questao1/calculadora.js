ecaa536d404aa2352fa727dc678cec7faf33bb7e
irn64 <gabriel.for3@outlook.com>
2025-03-12 21:41:08 -0300

    Comittando as respostas

---
+++ Atividade04/calculadora-projeto/Questao1/calculadora.js
@@ -0,0 +1,25 @@
+export function soma(num1, num2){
+    return num1 + num2
+}
+
+export function subtracao(num1, num2){
+    return num1 - num2
+}
+
+export function multiplicacao(num1, num2){
+    return num1 * num2
+}
+
+export function divisao(num1, num2){
+    return num1 / num2
+}
+
+export function calcularIdade(anoNascimento) {
+    if (idade < 18) {
+        console.log("Menor de idade");
+    } else if (idade >= 65) {
+        console.log("Idoso");
+    } else {
+        console.log("Maior de idade");
+    }
+    }
