feba97c4d186f7302dc47fdcd0010c3d5849cd82
irn64 <gabriel.for3@outlook.com>
2025-03-19 20:44:47 -0300

    Commitando a aula

--- meu-projeto/App.js
+++ meu-projeto/App.js
@@ -1,6 +1,6 @@
 // Imports
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native';
 
 // Componente principal
 // Ele deve retornar o que será renderizado na tela (Templete feito com JSX)
@@ -14,35 +14,56 @@ function alerta() {
 
   // Rotorno com JSX
   return (
+
+    <ScrollView>
+
     <View style={styles.container}>
     <StatusBar style="auto" />
-      {/* Comentário dentro do template JSX */}
+
 
       {/* Código JavaScript */}
     <Text>{2 + 2}</Text>
     <Text>{nome}</Text>
 
 
-      <Text>É isso ai</Text>
+      <Text style={{ fontSize: 40 }}>É isso ai</Text>
+      <Text style={styles.textoGrande}>Eai minha rapaziada</Text>
+
       <Button title='Alerta' onPress={alerta}></Button>
       
       <Image 
-      source={{uri: 'https://agorarn.com.br/files/uploads/2023/03/Neymar-930x524.jpg'}}
-      style= {{
-        height: 300,
-        width: 300,
+        source={{uri: 'https://agorarn.com.br/files/uploads/2023/03/Neymar-930x524.jpg'}}
+        style= {{
+          height: 300,
+          width: 300,
+      }}
+      />
+
+      <Image 
+        source={require('./Imagens/neyma.png')}
+        style= {{
+          height: 300,
+          width: 300,
       }}
       />
       
     </View>
+    </ScrollView>
   );
 }
+   
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 50
   },
+  textoGrande: {
+    fontSize: 50,
+    fontWeight: 900,
+    fontFamily: 'italic'
+  }
 });
