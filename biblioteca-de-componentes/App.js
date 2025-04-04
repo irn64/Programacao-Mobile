6056264f82589a1d7c8a78a23297c7f65190e774
irn64 <gabriel.for3@outlook.com>
2025-04-03 21:25:20 -0300

    Commitando a atividade07

--- biblioteca-de-componentes/App.js
+++ biblioteca-de-componentes/App.js
@@ -1,52 +1,122 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, View, Flatlist } from 'react-native';
-import { PaperProvider, Card, Title, Paragraph, Text, Button, Divider} from 'react-native-paper'
+import React from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
+import { PaperProvider, Text } from 'react-native-paper';
+import Estado from './componentes/Estado';
 
 export default function App() {
 
-  const listaCards = [
+  const listaEstadosMunicipios = [
     {
-      titulo: "Card 1",
-      descricao: "Lorem Ipsun, Lorem Ipsun, Lorem Ipsun, Lorem Ipsun, Lorem Ipsun",
-      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg533J0TIP7a6z-C_XrSUTz2SKBXxPKRiTyA&s'
+      nome: 'Rio de Janeiro',
+      sigla: 'RJ',
+      imagem: 'https://picsum.photos/700',
+      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      municipios: [
+        {
+          nome: 'Rio de Janeiro',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Niterói',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Petrópolis',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Angra dos Reis',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Cabo Frio',
+          imagem: 'https://picsum.photos/700'
+        }
+      ]
     },
     {
-      titulo: "Card 1",
-      descricao: "Lorem Ipsun, Lorem Ipsun, Lorem Ipsun, Lorem Ipsun, Lorem Ipsun",
-      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg533J0TIP7a6z-C_XrSUTz2SKBXxPKRiTyA&s'
+      nome: 'São Paulo',
+      sigla: 'SP',
+      imagem: 'https://picsum.photos/700',
+      descricao: 'São Paulo é o estado mais populoso do Brasil, com uma economia diversificada e forte.',
+      municipios: [
+        {
+          nome: 'São Paulo',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Campinas',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Santos',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Sorocaba',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Ribeirão Preto',
+          imagem: 'https://picsum.photos/700'
+        }
+      ]
     },
     {
-      titulo: "Card 1",
-      descricao: "Lorem Ipsun, Lorem Ipsun, Lorem Ipsun, Lorem Ipsun, Lorem Ipsun",
-      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg533J0TIP7a6z-C_XrSUTz2SKBXxPKRiTyA&s'
-    },
-    {
-      titulo: "Card 1",
-      descricao: "Lorem Ipsun, Lorem Ipsun, Lorem Ipsun, Lorem Ipsun, Lorem Ipsun",
-      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg533J0TIP7a6z-C_XrSUTz2SKBXxPKRiTyA&s'
+      nome: 'Minas Gerais',
+      sigla: 'MG',
+      imagem: 'https://picsum.photos/700',
+      descricao: 'Minas Gerais é conhecido por sua rica história, culinária e belas paisagens.',
+      municipios: [
+        {
+          nome: 'Belo Horizonte',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Ouro Preto',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Uberlândia',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Juiz de Fora',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Montes Claros',
+          imagem: 'https://picsum.photos/700'
+        }
+      ]
     }
   ]
+
 
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <StatusBar style='auto' />
+        <StatusBar style="auto" />
 
-        <Flatlist 
-          data={listaCards}
-          renderItem={({item}) => (
-            <Card>
-              <Card.Content>
-                <Title>{item.titulo}</Title>
-                <Paragraph>{item.descricao}</Paragraph>
-              </Card.Content>
-              <Card.Cover source={{uri: item.imagem}} />
-            </Card>
+        <Text variant='displaySmall'>Lista de Estados</Text>
+
+        <FlatList
+          data={listaEstadosMunicipios}
+          renderItem={({ item }) => (
+            <Estado
+              nome={item.nome}
+              sigla={item.sigla}
+              descricao={item.descricao}
+              imagem={item.imagem}
+              municipios={item.municipios}
+            />
           )}
         />
 
+
       </View>
-       </PaperProvider>
+    </PaperProvider>
   );
 }
 
@@ -56,6 +126,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 50
+    paddingTop: 20
   },
 });
